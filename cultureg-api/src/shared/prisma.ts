a3e771e21f00037b54971430f51c05fce7ddd4e7
TonyGH6 <antoine.de.vinck@gmail.com>
2026-02-15 04:49:28 +0100

    feat: switch to Prisma Accelerate (remove local pg adapter, use PRISMA_URL)

--- cultureg-api/src/shared/prisma.ts
+++ cultureg-api/src/shared/prisma.ts
@@ -1,12 +1,3 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
-import { env } from "../env";
 
-const pool = new Pool({
-    connectionString: env.DATABASE_URL,
-});
-
-const adapter = new PrismaPg(pool);
-
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
